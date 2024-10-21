e6e8440fa01d38cf7a4af023124bb1f8079b06f7
Dana Brunson <166191666+danabrunson@users.noreply.github.com>
2024-10-21 15:08:16 -0400

    Update journal.tsx
    Created text area in journal to allow input. (Currently only allows one line)

--- scholars-serenity-mobile-app/app/journal.tsx
+++ scholars-serenity-mobile-app/app/journal.tsx
@@ -1,5 +1,25 @@
-import { Text } from 'react-native';
+import React from 'react';
+import { View, Text,TextInput } from 'react-native';
 
-export default function Journal() {
-    
-}
+const JournalInput = () => {
+    const [value, onChangeText] = React.useState('Start Typing here');
+    return (
+        <View
+            style={{
+                backgroundColor: value,
+                borderBottomColor: 'transparent',
+                borderBottomWidth: 1,
+              }}>
+              <TextInput
+                editable={true}
+                multiline={true}
+                numberOfLines={Infinity}
+                onChangeText={text => onChangeText(text)}
+                value={value}
+                style={{padding: 10}}
+              />
+            </View>
+    );
+};
+
+export default JournalInput;
