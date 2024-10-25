896dada94986adeb4a517af66cf9e0fb5e8ed164
Dana Brunson <166191666+danabrunson@users.noreply.github.com>
2024-10-25 16:11:21 -0400

    Update index.tsx

--- scholars-serenity-mobile-app/app/(tabs)/index.tsx
+++ scholars-serenity-mobile-app/app/(tabs)/index.tsx
@@ -1,31 +1,40 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View,  StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Scholar's Serenity</Text>
-      <Link href="/guide" style={styles.button}>
-        Continue to guide
-      </Link>
-    </View>
+      <Text style={styles.welcomeMsg1}>Scholar's</Text>
+      <Text style={styles.welcomeMsg2}>Serenity</Text>
+      <Text style={styles.text}>Take a deep breath and stay awhile!</Text>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f7b9d7',
+    backgroundColor: '#fafafa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcomeMsg1: {
+    position: 'absolute',
+    top: 40,
+    fontSize: 60,
+    color: '#8bb4cf',
+  },
+  welcomeMsg2: {
+    position: 'absolute',
+    top: 87,
+    fontSize: 60,
+    color: '#8bb4cf',
+  },
   text: {
-    color: '#000000',
+    color: '#8bb4cf',
   },
   button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#000000',
-  },
+    fontSize: 10,
+    color: '#030303',
+  }
 });
-
