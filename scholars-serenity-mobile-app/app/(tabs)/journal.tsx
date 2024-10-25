3c8f8074c1bed20ab0b9364b2bfd3d12556f7412
Dana Brunson <166191666+danabrunson@users.noreply.github.com>
2024-10-25 15:33:16 -0400

    Update journal.tsx

--- scholars-serenity-mobile-app/app/(tabs)/journal.tsx
+++ scholars-serenity-mobile-app/app/(tabs)/journal.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import { View, Text,TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, StyleSheet, Pressable } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const JournalInput = () => {
-    const [value, onChangeText] = React.useState('Start Typing here');
     return (
-        <View
+        <SafeAreaView
             style={{
-                backgroundColor: value,
-                borderBottomColor: 'transparent',
-                borderBottomWidth: 1,
+              paddingTop: 20,
+              flex: 1, 
+              borderBottomColor: 'transparent',
+              borderBottomWidth: 1,
+              alignItems: 'center'
               }}>
               <TextInput
-                editable={true}
-                multiline={true}
-                numberOfLines={Infinity}
-                onChangeText={text => onChangeText(text)}
-                value={value}
-                style={{padding: 10}}
+              style={style.input}
+              keyboardType='default'
+              placeholder='Start typing here...'
+              placeholderTextColor='gray'
+              multiline={true}
               />
-            </View>
+              <Pressable style={style.button}>
+                <Text style={style.text}>Save</Text>
+              </Pressable>
+            </SafeAreaView>
     );
-};
+}; 
+
+const style = StyleSheet.create({
+input: {
+  paddingTop: 10,
+  paddingBottom: 10,
+  paddingHorizontal: 10,
+  width: 500,
+  height: 400,
+  borderRadius: 5,
+  borderWidth: 1,
+},
+button: {
+  backgroundColor: '#8bbcdf',
+  paddingVertical: 10,
+  paddingHorizontal: 32,
+  borderRadius: 4,
+  elevation: 3
+},
+text: {
+  fontSize: 16,
+  lineHeight: 21,
+  letterSpacing: 0.25,
+  color: 'white',
+}
+});
 
 export default JournalInput;
+
